aed1f010c1b84649c22304264cf0ae5daeb8070b
ray <ray@lzt>
2018-12-10 20:57:39 +0100

    simple led on shield

---
+++ mfs_leds1.ino
@@ -0,0 +1,34 @@
+// Multi Function Shield fuer Andruino, Make Heft 2/2018
+
+const uint8_t LED1 = 13;
+const uint8_t LED2 = 12;
+const uint8_t LED3 = 11;
+const uint8_t LED4 = 10;
+const uint16_t SPEED = 300;
+
+void setup()
+{
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED1, LOW);
+  delay(SPEED);
+  digitalWrite(LED1, HIGH);
+  
+  digitalWrite(LED2, LOW);
+  delay(SPEED);
+  digitalWrite(LED2, HIGH);
+  
+  digitalWrite(LED3, LOW);
+  delay(SPEED);
+  digitalWrite(LED3, HIGH);
+  
+  digitalWrite(LED4, LOW);
+  delay(SPEED);
+  digitalWrite(LED4, HIGH);
+}
